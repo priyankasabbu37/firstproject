3bf92681ed12926b29091293d40813affce08993
priyankasabbu37 <priyankasabbu37@gmail.com>
2019-02-09 13:34:58 +0530

    firstcommit # Please enter the commit message for your changes. Lines starting

---
+++ src/firstproject/demo.java
@@ -0,0 +1,11 @@
+package firstproject;
+
+public class demo {
+
+	public static void main(String[] args) {
+		System.out.println("hi..............");
+		System.out.println("hi..............");
+
+	}
+
+}
